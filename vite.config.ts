6194b0c100c66c60a9ae058fc1fb3283de0a05e4
Adarsh Tripathi <adarshtiwari1998@gmail.com>
2025-05-25 23:33:27 +0530

    change vite.config.ts file v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default defineConfig({
-  root: path.resolve(__dirname, "client"),
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@db": path.resolve(__dirname, "db"),
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
-    },
-  },
-  build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
-  },
-});
-
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
 // import path from "path";
@@ -34,17 +9,42 @@ export default defineConfig({
 // export default defineConfig({
 //   root: path.resolve(__dirname, "client"),
 //   plugins: [react()],
-//   server: {
-//     port: 7000,
-//   },
-//   build: {
-//     outDir: path.resolve(__dirname, "dist"),
-//     emptyOutDir: true,
-//   },
 //   resolve: {
 //     alias: {
-//       "@": path.resolve(__dirname, "client/src"),
+//       "@db": path.resolve(__dirname, "db"),
+//       "@": path.resolve(__dirname, "client", "src"),
+//       "@shared": path.resolve(__dirname, "shared"),
+//       "@assets": path.resolve(__dirname, "attached_assets"),
 //     },
 //   },
+//   build: {
+//     outDir: path.resolve(__dirname, "dist/public"),
+//     emptyOutDir: true,
+//   },
 // });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  root: path.resolve(__dirname, "client"),
+  plugins: [react()],
+  server: {
+    port: 7000,
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client/src"),
+    },
+  },
+});
 
