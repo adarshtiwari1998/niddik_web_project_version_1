19c5380eb9a1f758785f67c18fa0927c4030a293
Adarsh Tripathi <70283083+adarshtiwari1998@users.noreply.github.com>
2025-05-28 19:28:09 +0000

    update the logic  and add throw error logic

--- db/index.ts
+++ db/index.ts
@@ -22,7 +22,11 @@ import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from "@shared/schema";
 
-const DATABASE_URL = process.env.DATABASE_URL;
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL must be set. Did you forget to provision a database?",
+  );
+}
 
 const pool = new Pool({
   connectionString: DATABASE_URL,
